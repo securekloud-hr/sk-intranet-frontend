59808bb2818586f77e79c6a2494ad8ce10e5ba8e
securekloud-hr <mukund.subbaiah@securekloud.com>
2025-12-01 12:52:35 +0530

    test

--- src/Pages/HR.tsx
+++ src/Pages/HR.tsx
@@ -4363,7 +4363,7 @@ else if (fileName === "Letter_of_Undertaking(2).pdf") {
                   >
                     {/* Top section with initials */}
  
-                   <img src="../../public/collapse.png"></img>
+                   <img src="../../public/collapse.png" className="w-10 h-10"></img>
                     {/* Employee details */}
                     <div className="p-4 text-sm space-y-1">
                       {/* ✅ Shown to everyone */}
